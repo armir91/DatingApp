a3360d3fe8e4c6438990667b9ee49010bcb38d1b
Armir <armir.keta@hotmail.com>
2024-01-04 11:18:36 +0100

    Initial Commit - EoS 2

---
+++ API/Program.cs
@@ -0,0 +1,22 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+
+builder.Services.AddDbContext<API.Data.DataContext>(Opt => {
+    Opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+var app = builder.Build();
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();

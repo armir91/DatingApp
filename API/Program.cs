6e17e732e5ee3ca9084326ecb192c6f05d02982b
Armir <armir.keta@hotmail.com>
2024-01-05 16:37:42 +0100

    Angular Configuration - EoS 3

--- API/Program.cs
+++ API/Program.cs
@@ -7,13 +7,24 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 
-builder.Services.AddDbContext<API.Data.DataContext>(Opt => {
+builder.Services.AddDbContext<API.Data.DataContext>(Opt => 
+{
     Opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+builder.Services.AddCors(opt =>
+{
+    opt.AddPolicy("CorsPolicy", policy =>
+    {
+        policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200");
+    });
 });
 
 var app = builder.Build();
 
 app.UseHttpsRedirection();
+
+app.UseCors("CorsPolicy");
 
 app.UseAuthorization();
 
